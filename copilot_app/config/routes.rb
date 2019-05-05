9d130432375f4494fcceae4b533f69d9446eaad1
Ed Roesch <edroesch@gmail.com>
2019-05-04 21:20:43 -0400

    added React client and adj some files for auth

--- copilot_app/config/routes.rb
+++ copilot_app/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :activities
+  resources :goals
+  post '/auth/login', to: 'authentication#login'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  
 end
